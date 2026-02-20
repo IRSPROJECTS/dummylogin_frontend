541bb1e8270afe45632af350c5f0d0f126cbacd3
IRSPROJECTS <ckarounias@innovation-res.eu>
2026-02-20 11:34:59 +0200

    adds api route

--- my-app/src/api.js
+++ my-app/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'web-production-118b9.up.railway.app' 
+  baseURL: 'web-production-10396.up.railway.app' 
 });
 
 export default API;
